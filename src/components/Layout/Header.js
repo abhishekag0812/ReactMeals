c0b4b7d716bebd3709956c89beabe23b9f36bf3b
AbhishekAg812 <abhishek.agrawal1@oyorooms.com>
2021-05-04 20:15:30 +0530

    header ready

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React , {Fragment} from 'react';
 import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = () => {
 
@@ -9,7 +10,7 @@ const Header = () => {
         <Fragment>
             <header className = {classes.header}>
                 <h1>ReactMeals</h1>
-                <button>cart</button>
+                <HeaderCartButton/>
             </header>
             <div>
                 <img src = {mealsImage} className = {classes['main-image']} alt = "A table full of delicious food"/>
